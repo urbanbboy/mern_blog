8a54fef82f30037f2a3be09ffe8ca33a0ecf5745
talant <talant.04asanov@gmail.com>
2023-06-02 22:11:16 +0600

    fix: refactored extraReducers in postSlice.js

--- client/src/redux/features/post/postSlice.js
+++ client/src/redux/features/post/postSlice.js
@@ -1,4 +1,4 @@
-import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk, isPending } from '@reduxjs/toolkit'
 import axios from '../../../utils/axios'
 
 const initialState = {
@@ -56,58 +56,52 @@ export const postSlice = createSlice({
     name: 'post',
     initialState,
     reducers: {},
-    extraReducers: {
-        // Создание поста
-        [createPost.pending]: (state) => {
-            state.loading = true
-        },
-        [createPost.fulfilled]: (state, action) => {
-            state.loading = false
-            state.posts.push(action.payload)
-        },
-        [createPost.rejected]: (state) => {
-            state.loading = false
-        },
-        // Получаение всех постов
-        [getAllPosts.pending]: (state) => {
-            state.loading = true
-        },
-        [getAllPosts.fulfilled]: (state, action) => {
-            state.loading = false
-            state.posts = action.payload.posts
-            state.popularPosts = action.payload.popularPosts
-        },
-        [getAllPosts.rejected]: (state) => {
-            state.loading = false
-        },
-        // Удаление поста
-        [removePost.pending]: (state) => {
-            state.loading = true
-        },
-        [removePost.fulfilled]: (state, action) => {
-            state.loading = false
-            state.posts = state.posts.filter(
-                (post) => post._id !== action.payload._id,
-            )
-        },
-        [removePost.rejected]: (state) => {
-            state.loading = false
-        },
-        // Обновление поста
-        [updatePost.pending]: (state) => {
-            state.loading = true
-        },
-        [updatePost.fulfilled]: (state, action) => {
-            state.loading = false
-            const index = state.posts.findIndex(
-                (post) => post._id === action.payload._id,
-            )
-            state.posts[index] = action.payload
-        },
-        [updatePost.rejected]: (state) => {
-            state.loading = false
-        },
-    },
+    extraReducers: (builder) => {
+        builder
+            //create Post
+            .addCase(createPost.pending, (state) => {
+                state.loading = true
+            })
+            .addCase(createPost.fulfilled, (state, action) => {
+                state.loading = false
+                state.posts.push(action.payload)
+            })
+            .addCase(createPost.rejected, (state) => {
+                state.loading = false
+            })
+            //get all Posts
+            .addCase(getAllPosts.pending, (state) => {
+                state.loading = true
+            })
+            .addCase(getAllPosts.fulfilled, (state, action) => {
+                state.loading = false
+                state.posts = action.payload.posts
+                state.popularPosts = action.payload.popularPosts
+            })
+            .addCase(getAllPosts.rejected, (state) => {
+                state.loading = false
+            })
+            //remove Post
+            .addCase(removePost.pending, (state) => {
+                state.loading = true
+            })
+            .addCase(removePost.fulfilled, (state, action) => {
+                state.loading = false
+                state.posts = state.posts.filter(
+                    (post) => post._id !== action.payload._id,
+                )
+            })
+            .addCase(removePost.rejected, (state) => {
+                state.loading = false
+            })
+            //update post
+            .addCase(updatePost.pending, (state) => {
+                state.loading = true
+            })
+            .addCase(updatePost.fulfilled, (state, action) => {
+
+            })
+    }
 })
 
 export default postSlice.reducer
